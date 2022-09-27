98c6be5f384308ce22018ba698d376d420cdec87
Aishwary Patle <61194375+Aishwarypatle@users.noreply.github.com>
2022-09-27 20:02:53 +0000

    added:books database

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,47 @@
 import './App.css'
+import { useState } from 'react';
+import React from 'react';
+
+
+let booksList =
+  {
+    Sci- Fi =
+  [
+    { name:"Beyond the burn line", author:"Paul McAuley"},
+    { name:"Expect me tommmorow", author:"Christopher priest"},
+    { name:"Isolation", author:"Dan Coxon"},
+    { name:"The Pillars of the Earth", author:"Ken Follett"}
+  ],
+
+  Horror =
+    [
+      { name: "Black Mouth", author: "Ronald Malfi" },
+      { name: "The Stage Mother's Club", author: "Ran Copshaw" },
+      { name: "All that's lost", author: "Ray cluley" },
+      { name: "Ricky's Hand", author: "David Quantick" }
+    ],
+  Action & Adeventure =
+[
+  { name: "Jurassic Park ", author: "Micheal Crichton" },
+  { name: "The Hobbit", author: "J.R.R. Tolkien" },
+  { name: "The Hunger Games", author: "Suzane collins" },
+  { name: "The bourne identity", author: " Robert Ludlum" }
+],
+  Philosophy =
+  [
+    { name: "A History of Western Philosophy ", author: " Bertrand Russell" },
+    { name: "The Philosophy of Andy Warhol", author: " Andy Warhol" },
+    { name: "The Consolations of Philosophy", author: " Alain de Botton" },
+    { name: "Meditations on First Philosophy", author: "René Descartes" }
+  ],
+}
+
+
+
+
+
+
+
 
 export default function App() {
   return (
